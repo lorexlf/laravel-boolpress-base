028363a37164af4b00951fb9b7bb0a3d4fe3f446
Lorenzo <lorenzo.franzone@gmail.com>
2020-03-25 16:06:24 +0100

    Post >> migrate1

--- database/migrations/2020_03_25_145402_create_posts_table.php
+++ database/migrations/2020_03_25_145402_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longtext('text');
+            $table->string('img');
             $table->timestamps();
         });
     }
