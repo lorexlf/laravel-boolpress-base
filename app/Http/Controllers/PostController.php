64ccf277797dffce5d5637f5837235b1ba96f077
Lorenzo <lorenzo.franzone@gmail.com>
2020-03-25 15:58:17 +0100

    Laravel install + Post routes/controller/model-migration1

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+
+    // --------------------------------------------------
+    // I N D E X
+    // --------------------------------------------------
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    // --------------------------------------------------
+    // C R E A T E
+    // --------------------------------------------------
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    // --------------------------------------------------
+    // S T O R E
+    // --------------------------------------------------
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    // --------------------------------------------------
+    // S H O W
+    // --------------------------------------------------
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    // --------------------------------------------------
+    // E D I T
+    // --------------------------------------------------
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    // --------------------------------------------------
+    // U P D A T E
+    // --------------------------------------------------
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    // --------------------------------------------------
+    // D E S T R O Y
+    // --------------------------------------------------
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
